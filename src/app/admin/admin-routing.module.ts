070cf6c8686d153f12743fcd8421d1acf46b8c9f
nitintathe96 <nitintathe96@gmail.com>
2022-06-20 14:40:04 +0530

    compleded angular routing with different module and also apply lazy loading to nested module

---
+++ src/app/admin/admin-routing.module.ts
@@ -0,0 +1,28 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin.component';
+import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+
+const routes: Routes =[
+{
+  path:'',
+  component:AdminComponent,
+  children:[{ path:'customer',
+  loadChildren:()=>import('../admin/customer/customer.module').then((m)=>m.CustomerModule)}]
+}
+];
+// {
+//   path:'header',
+//   component:HeaderComponent
+// },
+// {
+//   path:'side-bar',
+//   component:SideBarComponent
+// },
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)], 
+  exports: [RouterModule]
+})
+export class AdminRoutingModule {}
